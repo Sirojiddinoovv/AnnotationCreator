de8f11a8261f0384745b046017cf96e987ddf9f0
Home-PC <davrbankit@gmail.com>
2025-02-15 20:21:40 +0500

    take message from param

--- src/main/java/uz/nodir/beanregisterdemo/endpoint/TestController.java
+++ src/main/java/uz/nodir/beanregisterdemo/endpoint/TestController.java
@@ -2,9 +2,13 @@ package uz.nodir.beanregisterdemo.endpoint;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uz.nodir.beanregisterdemo.service.business.impl.UniqueRequestServiceImpl;
+
+import java.util.Map;
 
 /**
  * @author: PC
@@ -18,7 +22,12 @@ public class TestController {
     private final UniqueRequestServiceImpl uniqueRequestService;
 
     @GetMapping
-    public void test() {
-        uniqueRequestService.sendRequestToThirdParty("Test");
+    public ResponseEntity<?> request(@RequestParam("message") String message) {
+        uniqueRequestService.sendRequestToThirdParty(message);
+        return ResponseEntity.ok(
+                Map.of(
+                        "result", "SUCCESS"
+                )
+        );
     }
 }
